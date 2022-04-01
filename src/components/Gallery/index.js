765383c3bc241a96b38ed630792ed20128efeb1a
Ian Linton <ilinton2571@gmail.com>
2022-04-01 15:36:41 -0700

    broke something cuz the instructions are SUPER CLEAR

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
 import PhotoList from "../PhotoList";
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Gallery({ currentCategory }) {
-    const { name, description } = currentCategory;
+function Gallery(props) {
+    const currentCategory = props;
     return (
         <section>
             <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-            <p>{description</p>
-            <PhotoList />
+            <p>{description}</p>
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 }
