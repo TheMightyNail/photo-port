989af2c25c65474c099f9b7357d6a8311983f653
Ian Linton <ilinton2571@gmail.com>
2022-04-02 13:53:07 -0700

    add modal component

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 // import photo from '../../assets/small/commercial/0.jpg';
-// import Modal from '../Modal';
+import Modal from '../Modal';
+
+
 
 const PhotoList = ({ category }) => {
+    const [currentPhoto, setCurrentPhoto] = useState();
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [photos] = useState([
         {
           name: 'Grocery aisle',
@@ -100,17 +104,24 @@ const PhotoList = ({ category }) => {
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
         },
       ]);
-
+      
       const currentPhotos = photos.filter((photo) => photo.category === category);
+
+      const toggleModal = (image, i) => {
+        setCurrentPhoto({...image, index: i});
+        setIsModalOpen(true);
+      }
 
     return (
         <div>
+            {isModalOpen && <Modal currentPhoto={currentPhoto} />}
             <div className='flex-row'>
                 {currentPhotos.map((image, i) => (
                 <img
                     src={require(`../../assets/small/${category}/${i}.jpg`)}
                     alt={image.name}
                     className="img-thumbnail mx-1"
+                    onClick={() => toggleModal(image, i)}
                     key={image.name}
                     />
                 ))}
